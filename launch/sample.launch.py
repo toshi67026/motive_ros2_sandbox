229c648786eb4dc438dec8e274e220098e24ed32
Toshiyuki Oshima <toshiyuki67026@gmail.com>
2022-06-07 15:13:00 +0900

    update sample launch

--- launch/sample.launch.py
+++ launch/sample.launch.py
@@ -1,24 +1,39 @@
-from pathlib import Path
+#!/usr/bin/env python3
 
-import launch_ros.actions
+import os
+
+from launch_ros.actions import Node
 from ament_index_python.packages import get_package_share_directory
 
-import launch
-import launch.actions
-import launch.substitutions
-
-parameters_file_path = Path(get_package_share_directory("vrpn_client_ros"), "config", "sample.params.yaml")
+from launch import LaunchDescription
+from launch.substitutions import LaunchConfiguration
 
 
-def generate_launch_description():
-    return launch.LaunchDescription(
-        [
-            launch_ros.actions.Node(
-                package="vrpn_client_ros",
-                executable="vrpn_client_node",
-                output="screen",
-                emulate_tty=True,
-                parameters=[parameters_file_path],
-            ),
-        ]
+def generate_launch_description() -> LaunchDescription:
+    pkg_dir = get_package_share_directory("motive_ros_foxy_sandbox")
+
+    sample_params_path = os.path.join(pkg_dir, "sample.params.yaml")
+    rviz_config_path = os.path.join(pkg_dir, "sample.rviz")
+
+    vrpn_client_node = Node(
+        package="vrpn_client_ros",
+        executable="vrpn_client_node",
+        output="screen",
+        parameters=[sample_params_path],
     )
+
+    rviz2_node = Node(
+        package="rviz2",
+        executable="rviz2",
+        name="rviz2",
+        output="screen",
+        arguments=["-d", rviz_config_path],
+    )
+
+    # Create the launch description and populate
+    ld = LaunchDescription()
+
+    ld.add_action(vrpn_client_node)
+    ld.add_action(rviz2_node)
+
+    return ld
